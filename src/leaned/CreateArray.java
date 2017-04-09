16110bed97cfacdfb73dd97dead0875e9ce1f99a
Md Abedin <mdabedinny@gmail.com>
2017-04-09 19:35:20 -0400

    New change

--- src/leaned/CreateArray.java
+++ src/leaned/CreateArray.java
@@ -4,4 +4,12 @@ package leaned;
  * Created by mdislam on 4/9/17.
  */
 public class CreateArray {
+    public static void main(String[] args) {
+
+        int [] numArray = {4, 5, 6, 8,10};
+        String [] name = {"Topu", "Khan", "Abedin", "Raihan", "Sayan"};
+        System.out.println("Hello Mithu!");
+    }
+
+
 }
